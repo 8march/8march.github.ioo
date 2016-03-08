7c7ca73c69c5b19e274df53c57c2aa7fee2f3ac0
Alexey Kunitsky <anonymouss.alex@gmail.com>
2016-03-08 11:43:30 +1000

    Add some shit
    Add fonts, overlay hide, select page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var i;
 function image_click(sender){
-  document.getElementById('overlay').style.display = "block";
+  document.getElementById('overlay-container').style.display = "block";
   document.getElementById('password').value = "";
   i = sender;
   // overlay.style.display = "block";
@@ -10,6 +10,11 @@ function image_click(sender){
 function check(){
   if(i.id == document.getElementById('password').value){
     i.style.display = "none"}
-  else alert("не угадали)))");
-  document.getElementById('overlay').style.display = "none";
+  else alert("Неверный пароль");
+  document.getElementById('overlay-container').style.display = "none";
+}
+
+function overlayHide(sender){
+    document.getElementById('overlay-container').style.display = "none";
+
 }
